3193d9b1a2ecda1fbba1bf63ff71c04c3203cd15
AnuchitO <>
2022-10-06 10:43:26 +0700

    refactor

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,17 +1,11 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(num int) string {
 	if num == 3 {
 		return "Fizz"
 	}
 
-	if num == 4 {
-		return "4"
-	}
-
-	if num == 2 {
-		return "2"
-	}
-
-	return "1"
+	return fmt.Sprintf("%d", num)
 }
