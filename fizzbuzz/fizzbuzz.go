364bcc3178e249433654d2fbb86262daa8358ce6
AnuchitO <>
2022-10-06 11:44:51 +0700

    fizzbuzz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,13 +3,29 @@ package fizzbuzz
 import "fmt"
 
 func FizzBuzz(num int) string {
-	if num == 5 {
+	if isFizzBuzz(num) {
+		return "FizzBuzz"
+	}
+
+	if isBuzz(num) {
 		return "Buzz"
 	}
 
-	if num == 3 || num == 6 {
+	if isFizz(num) {
 		return "Fizz"
 	}
 
 	return fmt.Sprintf("%d", num)
+}
+
+func isFizzBuzz(num int) bool {
+	return num%15 == 0
+}
+
+func isFizz(num int) bool {
+	return num%3 == 0
+}
+
+func isBuzz(num int) bool {
+	return num%5 == 0
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -14,6 +14,16 @@ func TestFizzBuzz(t *testing.T) {
 		{name: "input 4 should be 4", input: 4, want: "4"},
 		{name: "input 5 should be Buzz", input: 5, want: "Buzz"},
 		{name: "input 6 should be Fizz", input: 6, want: "Fizz"},
+		{name: "input 7 should be 7", input: 7, want: "7"},
+		{name: "input 8 should be 8", input: 8, want: "8"},
+		{name: "input 9 should be Fizz", input: 9, want: "Fizz"},
+		{name: "input 10 should be Buzz", input: 10, want: "Buzz"},
+		{name: "input 11 should be 11", input: 11, want: "11"},
+		{name: "input 12 should be Fizz", input: 12, want: "Fizz"},
+		{name: "input 13 should be 13", input: 13, want: "13"},
+		{name: "input 14 should be 14", input: 14, want: "14"},
+		{name: "input 15 should be FizzBuzz", input: 15, want: "FizzBuzz"},
+		{name: "input 30 should be 30", input: 30, want: "FizzBuzz"},
 	}
 
 	for _, tt := range cases {
