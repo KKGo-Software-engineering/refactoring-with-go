a0d6cc9f1295608af1598cc1dca8f41986646429
AnuchitO <>
2022-10-06 10:37:47 +0700

    refactor test

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -4,20 +4,22 @@ import "testing"
 
 func TestFizzBuzz(t *testing.T) {
 	want := "1"
+	input := 1
 
-	got := FizzBuzz(1)
+	got := FizzBuzz(input)
 
 	if got != want {
-		t.Errorf("FizzBuzz(1) = %q, want %q", got, want)
+		t.Errorf("FizzBuzz(%d) = %q, want %q", input, got, want)
 	}
 }
 
 func TestInput2ShouldReturn2(t *testing.T) {
 	want := "2"
+	input := 2
 
-	got := FizzBuzz(2)
+	got := FizzBuzz(input)
 
 	if got != want {
-		t.Errorf("FizzBuzz(1) = %q, want %q", got, want)
+		t.Errorf("FizzBuzz(%d) = %q, want %q", input, got, want)
 	}
 }
