68a201c82be229040e665a6c21243573f49a93b2
AnuchitO <>
2022-10-06 10:36:43 +0700

    input 2 should be 2

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,5 +1,9 @@
 package fizzbuzz
 
 func FizzBuzz(num int) string {
+	if num == 2 {
+		return "2"
+	}
+
 	return "1"
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -11,3 +11,13 @@ func TestFizzBuzz(t *testing.T) {
 		t.Errorf("FizzBuzz(1) = %q, want %q", got, want)
 	}
 }
+
+func TestInput2ShouldReturn2(t *testing.T) {
+	want := "2"
+
+	got := FizzBuzz(2)
+
+	if got != want {
+		t.Errorf("FizzBuzz(1) = %q, want %q", got, want)
+	}
+}
