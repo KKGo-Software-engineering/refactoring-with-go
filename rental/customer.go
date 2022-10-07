68261b6cc5ff2c95af7442dfbedaa5c1ac4d5b8b
AnuchitO <>
2022-10-07 09:52:46 +0700

    remove temporary fields

--- rental/customer.go
+++ rental/customer.go
@@ -41,13 +41,11 @@ func (r Rental) Charge() float64 {
 }
 
 func getPoints(r Rental) int {
-	frequentRenterPoints := 0
-	frequentRenterPoints++
 	if r.Movie().PriceCode() == NEW_RELEASE && r.DaysRented() > 1 {
-		frequentRenterPoints++
+		return 2
 	}
 
-	return frequentRenterPoints
+	return 1
 }
 
 func (rcvr Customer) Statement() string {
