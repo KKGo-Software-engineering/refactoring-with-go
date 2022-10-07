650704e92e522b60b0debae5d0a5a586fc95d05d
AnuchitO <>
2022-10-07 10:03:35 +0700

    remove temporary fields

--- rental/customer.go
+++ rental/customer.go
@@ -21,23 +21,24 @@ func (rcvr Customer) Name() string {
 }
 
 func (r Rental) Charge() float64 {
-	result := 0.0
 	switch r.Movie().PriceCode() {
 	case REGULAR:
-		result += 2
+		result := 2.0
 		if r.DaysRented() > 2 {
 			result += float64(r.DaysRented()-2) * 1.5
 		}
+		return result
 	case NEW_RELEASE:
-		result += float64(r.DaysRented()) * 3.0
+		return float64(r.DaysRented()) * 3.0
 	case CHILDRENS:
-		result += 1.5
+		result := 1.5
 		if r.DaysRented() > 3 {
 			result += float64(r.DaysRented()-3) * 1.5
 		}
+		return result
 	}
 
-	return result
+	return 0
 }
 
 func getPoints(r Rental) int {
@@ -48,16 +49,35 @@ func getPoints(r Rental) int {
 	return 1
 }
 
-func (rcvr Customer) Statement() string {
-	totalAmount := 0.0
-	frequentRenterPoints := 0
-	result := fmt.Sprintf("Rental Record for %v\n", rcvr.Name())
-	for _, r := range rcvr.rentals {
-		frequentRenterPoints += getPoints(r)
-		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), r.Charge())
-		totalAmount += r.Charge()
+func getTotalPoints(rentals []Rental) int {
+	points := 0
+	for _, r := range rentals {
+		points += getPoints(r)
+	}
+
+	return points
+}
+
+func getTotalAmount(rentals []Rental) float64 {
+	result := 0.0
+	for _, r := range rentals {
+		result += r.Charge()
+	}
+	return result
+}
+
+func (c Customer) Statement() string {
+	totalAmount := getTotalAmount(c.rentals)
+	points := getTotalPoints(c.rentals)
+
+	result := fmt.Sprintf("Rental Record for %v\n", c.Name())
+	for _, r := range c.rentals {
+		title := r.Movie().Title()
+		amount := r.Charge()
+
+		result += fmt.Sprintf("\t%v\t%.1f\n", title, amount)
 	}
 	result += fmt.Sprintf("Amount owed is %.1f\n", totalAmount)
-	result += fmt.Sprintf("You earned %v frequent renter points", frequentRenterPoints)
+	result += fmt.Sprintf("You earned %v frequent renter points", points)
 	return result
 }
